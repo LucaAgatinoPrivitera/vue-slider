586dfb70e0b2e2524f6d5919c9c7c36d833bc04c
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-14 10:35:32 +0200

    Milestone 3 - Adesso i pulsanti resettano l'immagine quando si sfora fuori dall'array

--- js/script.js
+++ js/script.js
@@ -1,27 +1,5 @@
 // Dichiaro l'array di oggetti
-const slides = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+
 
 // Creo l'app
 const { createApp } = Vue
@@ -29,8 +7,30 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
             // Output ha l'array e la variabile che indica che l'immagine attiva deve avere come indice 0
-            slides,
             activeImage: 0,
         }
     },
@@ -39,16 +39,37 @@ createApp({
         checkImageVisibility(indice) {
             return (indice == this.activeImage) ? 'active' : '';
         },
-        back(indice) {
+
+        back() {
             // indice = indice; Da chiedere
             // if (indice = 0){
-            this.activeImage--;
-            console.log(indice);
+            if (this.activeImage !== 0) {
+                this.activeImage--;
+            }
+            else {
+                this.activeImage = this.slides.length - 1
+            }
+            console.log(this.activeImage);
             // }
 
         },
+
         next() {
-            this.activeImage++;
+            if (this.activeImage !== 4) {
+                this.activeImage++;
+            }
+            else {
+                this.activeImage = 0;
+            }
         },
+        counter(i) {
+            return i;
+        }
     }
+
+
+
+
+
+
 }).mount('.slider-wrapper')
